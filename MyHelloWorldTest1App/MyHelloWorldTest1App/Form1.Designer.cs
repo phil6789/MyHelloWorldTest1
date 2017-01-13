12d3579e6eea8f14cc55eca7500559666e66dfbc
Phil6789 <ph.zoss@bluewin.ch>
2017-01-13 11:45:51 +0100

    first button test

--- MyHelloWorldTest1App/MyHelloWorldTest1App/Form1.Designer.cs
+++ MyHelloWorldTest1App/MyHelloWorldTest1App/Form1.Designer.cs
@@ -29,22 +29,55 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(87, 31);
+            this.label1.Location = new System.Drawing.Point(9, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.Size = new System.Drawing.Size(165, 13);
             this.label1.TabIndex = 0;
             this.label1.Text = "Hello World ---> change from web";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(284, 135);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 55);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(265, 103);
+            this.textBox1.TabIndex = 3;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(274, 83);
+            this.ClientSize = new System.Drawing.Size(371, 170);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "MyHelloWorldTest1App";
@@ -56,6 +89,9 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- MyHelloWorldTest1App/MyHelloWorldTest1App/Form1.cs
+++ MyHelloWorldTest1App/MyHelloWorldTest1App/Form1.cs
@@ -4,10 +4,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace MyHelloWorldTest1App
 {
     public partial class Form1 : Form
@@ -15,6 +15,37 @@ namespace MyHelloWorldTest1App
         public Form1()
         {
             InitializeComponent();
+        }
+
+        public void Meth1() {}
+        public void Meth2() {}
+        public void Meth3() {}
+
+        public void Meth4() {}
+
+        public void Meth5() {}
+
+        public void Meth6() {}
+
+
+        private void Meth20()
+        {
+            
+
+        }
+
+
+        public void Meth10() {}
+        public void Meth11() {}
+        public void Meth12() {}
+        public void Meth13() {}
+        public void Meth14() {}
+        public void Meth15() {}
+        public void Meth16() {}
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "Button Pressed\r\n";
         }
     }
 }
